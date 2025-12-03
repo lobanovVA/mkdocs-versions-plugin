22ccd344f0cc7f366b83c24d5d53afc324e7c66d
Victor <163309062+lobanovVA@users.noreply.github.com>
2025-12-03 21:31:43 +0300

    update setup 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mkdocs-versioning",
-    version="0.1.0",
+    version="0.1.1",
     description="Simple version selector for MkDocs",
     packages=find_packages(),
     include_package_data=True,
